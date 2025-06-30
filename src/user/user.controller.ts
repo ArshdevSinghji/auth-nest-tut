f37a89c9531db9ee7856073b695efa5728b08fed
ArshdevSingh <arshdevrajput@gmail.com>
2025-06-30 15:23:40 +0530

    protected-routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async findAll() {
     return this.userService.findAll();
